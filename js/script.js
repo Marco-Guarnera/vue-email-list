8266db14dc75a0c2e72229741889366b994dbe51
Marco-Guarnera <Darkaquon93@gmail.com>
2024-09-11 18:03:37 +0200

    Completed

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@
 const app = Vue.createApp({
     name: "Vue Email List",
     data: () => ({
-
+        emailsList: [],
+        apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail"
     }),
     computed: {},
-    methods: {}
+    methods: {
+        getRandomEmails() {
+            for (let i = 0; i < 10; i++) {
+                axios.get(this.apiUrl).then(response => {
+                    this.emailsList.push(response.data.response);
+                });
+            }
+        }
+    },
+    created() {
+        this.getRandomEmails();
+    }
 });
 
 app.mount("#app");
